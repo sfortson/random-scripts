37b669c05d6f088adcc16704c65666100b830c2e
Samuel Fortson <sfortson@fortsonconsulting.com>
2020-02-25 08:17:41 -0500

    Simple birthday problem script

---
+++ src/birthday_problem/birthday_problem.py
@@ -0,0 +1,22 @@
+import argparse
+
+
+def probability(num_people):
+    i = 0
+    p = 1.0
+    while i < num_people:
+        p = p * (1 - (float(i)/365))
+        i += 1
+    return 1.0 - p
+
+
+def main():
+    parser = argparse.ArgumentParser("Compute birthday problem.")
+    parser.add_argument('num_people')
+    args = parser.parse_args()
+
+    print 100 * probability(int(args.num_people))
+
+
+if __name__ == '__main__':
+    main()
