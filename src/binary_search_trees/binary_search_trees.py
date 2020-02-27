f84b81db36cbd4ad996c76f7228512f2ca963e21
Sam Fortson <sam.fortson@gmail.com>
2020-02-26 22:12:41 -0500

    Add classes and functions for binary search trees (#2)

---
+++ src/binary_search_trees/binary_search_trees.py
@@ -0,0 +1,44 @@
+def inorder_tree_walk(x):
+    if x is not None:
+        inorder_tree_walk(x.left)
+        print(x.key)
+        inorder_tree_walk(x.right)
+
+
+def tree_search(x, k):
+    if x is None or k == x.key:
+        return x
+    if k < x.key:
+        return tree_search(x.left, k)
+    return tree_search(x.right, k)
+
+
+def iterative_tree_search(x, k):
+    while x is not None and k != x.key:
+        if k < x.key:
+            x = x.left
+        else:
+            x = x.right
+    return x
+
+
+def tree_minimum(x):
+    while x.left is not None:
+        x = x.left
+    return x
+
+
+def tree_maximum(x):
+    while x.right is not None:
+        x = x.right
+    return x
+
+
+def tree_successor(x):
+    if x.right is not None:
+        return tree_minimum(x.right)
+    y = x.p
+    while y is not None and x == y.right:
+        x = y
+        y = y.p
+    return y
