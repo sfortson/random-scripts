9b0f2aacbfda658428fdd3fb62504efa77e02ee9
Sam Fortson <sam.fortson@gmail.com>
2020-03-03 22:24:02 -0500

    Chapter 6: Profile Page and Avatars

--- src/flask_mega_tutorial/microblog/app/forms.py
+++ src/flask_mega_tutorial/microblog/app/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 
 from app.models import User
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
 
 
 class LoginForm(FlaskForm):
