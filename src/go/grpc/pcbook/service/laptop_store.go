8541c4ddc53fa79da28496ae247361b21c4647e2
Sam Fortson <sam.fortson@gmail.com>
2023-04-17 19:37:16 -0400

    server streaming

--- src/go/grpc/pcbook/service/laptop_store.go
+++ src/go/grpc/pcbook/service/laptop_store.go
@@ -1,8 +1,10 @@
 package service
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"log"
 	"sync"
 
 	"github.com/jinzhu/copier"
@@ -16,6 +18,7 @@ type LaptopStore interface {
 	Save(laptop *pb.Laptop) error
 	// Find finds a laptop by ID
 	Find(id string) (*pb.Laptop, error)
+	Search(ctx context.Context, filter *pb.Filter, found func(laptop *pb.Laptop) error) error
 }
 
 // InMemoryLaptopStore stores laptop in memory
@@ -65,13 +68,85 @@ func deepCopy(laptop *pb.Laptop) (*pb.Laptop, error) {
 
 // Find finds a laptop by ID
 func (store *InMemoryLaptopStore) Find(id string) (*pb.Laptop, error) {
-    store.mutex.RLock()
-    defer store.mutex.RUnlock()
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
 
-    laptop := store.data[id]
-    if laptop == nil {
-        return nil, nil
-    }
+	laptop := store.data[id]
+	if laptop == nil {
+		return nil, nil
+	}
 
-    return deepCopy(laptop)
+	return deepCopy(laptop)
+}
+
+// Search searches for laptops with filter, returns one by one via the found function
+func (store *InMemoryLaptopStore) Search(
+	ctx context.Context,
+	filter *pb.Filter,
+	found func(laptop *pb.Laptop) error,
+) error {
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+
+	for _, laptop := range store.data {
+		if ctx.Err() == context.Canceled || ctx.Err() == context.DeadlineExceeded {
+			log.Print("context is cancelled")
+			return nil
+		}
+
+		if isQualified(filter, laptop) {
+			other, err := deepCopy(laptop)
+			if err != nil {
+				return err
+			}
+
+			err = found(other)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func isQualified(filter *pb.Filter, laptop *pb.Laptop) bool {
+	if laptop.GetPriceUsd() > filter.GetMaxPriceUsd() {
+		return false
+	}
+
+	if laptop.GetCpu().GetNumberCores() < filter.GetMinCpuCores() {
+		return false
+	}
+
+	if laptop.GetCpu().GetMinGhz() < filter.GetMinCpuGhz() {
+		return false
+	}
+
+	if toBit(laptop.GetRam()) < toBit(filter.GetMinRam()) {
+		return false
+	}
+
+	return true
+}
+
+func toBit(memory *pb.Memory) uint64 {
+	value := memory.GetValue()
+
+	switch memory.GetUnit() {
+	case pb.Memory_UNIT_BIT:
+		return value
+	case pb.Memory_UNIT_BYTE:
+		return value << 3 // 8 = 2^3
+	case pb.Memory_UNIT_KILOBYTE:
+		return value << 13 // 1024 * 8 = 2^10 * 2^3 = 2^13
+	case pb.Memory_UNIT_MEGABYTE:
+		return value << 23
+	case pb.Memory_UNIT_GIGABYTE:
+		return value << 33
+	case pb.Memory_UNIT_TERABYTE:
+		return value << 43
+	default:
+		return 0
+	}
 }
